dbc41e1ba69af32013ee5c639bfc3d1736b39587
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-06-21 15:01:18 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 class="bg-red-700">Hellow world!! lets build netflixGPT</h1>
+      
+    </>
+  )
+}
+
+export default App
