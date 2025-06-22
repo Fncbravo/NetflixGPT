1a782eec551de5e7a6300d1ee064b3235e1d11a5
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-06-22 20:03:27 +0530

    updated components

---
+++ src/components/Browse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Browse = () => {
+  return (
+    <div className='bg-amber-300'>
+      <h1 className='decoration-orange-500'>hello browse</h1>
+    </div>
+  )
+}
+
+export default Browse
