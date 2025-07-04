30c8fd7daea8365e182a57d072bdefd31be74d18
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-07-04 23:50:01 +0530

    implemented signIn signUp

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useRef } from 'react'
 import Header from './Header' 
 import { checkValidData } from "../utils/validate.js"
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase.js"
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice.js';
 
 const Login = () => {
 
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  
+  const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
 
@@ -28,7 +34,26 @@ const Login = () => {
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
+        updateProfile(user,{
+          displayName: name.current.value,
+          photoURL: "https://avatars.githubusercontent.com/u/118635012?v=4"
+        }).then(()=>{
+          const { uid, email, displayName, photoURL} = auth.currentUser;
+                  dispatch(
+                    addUser({
+                      uid: uid,
+                      email: email,
+                      displayName: displayName,
+                      photoURL: photoURL
+                    })
+                  ); 
+         navigate("/browse")
+          // ...
+        }).catch((error)=> {
+          setErrorMessage(error.message)
+        });   
         console.log(user);
+        navigate("/browse")
         // ...
       })
       .catch((error) => {
@@ -48,6 +73,7 @@ const Login = () => {
         // Signed in
         const user = userCredential.user;
         console.log(user);
+        navigate("/browse")
         // ...
       })
       .catch((error) => {
@@ -78,6 +104,7 @@ const Login = () => {
         </h1>
         {!isSignInForm && (
         <input 
+        ref={name}
         type="text" 
         placeholder='Full Name' 
         className='p-2 my-4 w-full bg-gray-700'
