28bd8a3e0f462050dcc4c0fce04ae48868860b82
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-07-04 23:48:57 +0530

    updated browse page

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Header from './Header'
 
 const Browse = () => {
+
   return (
     <div className='bg-amber-300'>
-      <h1 className='decoration-orange-500'>hello browse</h1>
+      <Header />
+      <h1 className='decoration-orange-500'>hello browse</h1> 
     </div>
   )
+
+
+
 }
 
 export default Browse
